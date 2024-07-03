6b6ac529cbebe68fa8517b477c37987e531c0f9a
Jayanth P <jayanthjayram6968@gmail.com>
2024-07-03 12:44:05 +0530

    onkwydown

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -5,14 +5,21 @@ import { useContext } from "react";
 const Search = () => {
 
   const {input, onSent, setInput, mode} = useContext(Context)
+  const handleEnter =(e)=>{
+    if(e.key === "Enter"){
+      onSent()
+    }
+
+  }
 
   return (
     <div className={`flex flex-col justify-center items-center fixed w-full bottom-0  z-10 ${!mode ? " bg-secondryl" : "bg-secondryd"}`}>
 
-      <div className="flex justify-center items-center w-full h-full">
+      <div className="flex items-center justify-center w-full h-full">
         <input type="text" placeholder="message gyana" className={`m-2 outline-none pl-2 py-2 rounded-lg w-4/6 md:w-2/5 ${!mode ? "bg-primaryl" : "bg-primaryd"} ${!mode ? "text-fntl" : "text-fntd"}`} onChange={(e) => {
 								setInput(e.target.value);
 							}}
+              onKeyDown={handleEnter}
 							value={input}
 							/>
         <div className={`rounded-lg ${!mode ? "bg-primaryl" : "bg-primaryd"} `}>
